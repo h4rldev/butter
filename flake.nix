5943516db61f529a15f9e0a1cabea015ee27756f
h4rl <h4rldev@proton.me>
2026-09-15 10:57:05 +0200

    feat: improved nix flake, used mold for link

--- flake.nix
+++ flake.nix
@@ -20,231 +20,123 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
       pversion = "0.1.0";
+
+      platformInputs = platform:
+        if platform == "wayland"
+        then [pkgs.wayland]
+        else [pkgs.libxcb];
+
+      mkButter = {
+        name,
+        profile,
+        artifact,
+        pc,
+        platform,
+        loader ? false,
+        debug ? false,
+      }: let
+        artifactStem =
+          pkgs.lib.removePrefix "lib"
+          (pkgs.lib.removeSuffix ".so"
+            (pkgs.lib.removeSuffix ".a" (baseNameOf artifact)));
+
+        htilsPkg =
+          if debug
+          then htils.packages.${system}.htils-debug-threadsafe
+          else htils.packages.${system}.htils-threadsafe;
+      in
+        pkgs.stdenv.mkDerivation {
+          pname = name;
+          version = pversion;
+
+          src = ./.;
+
+          nativeBuildInputs =
+            [
+              conjure.packages.${system}.default
+              pkgs.gcc
+              pkgs.mold
+              pkgs.vulkan-headers
+              htilsPkg
+            ]
+            ++ platformInputs platform
+            ++ pkgs.lib.optional loader pkgs.vulkan-loader;
+
+          buildPhase = ''
+            runHook preBuild
+            conjure as ${profile} build
+            runHook postBuild
+          '';
+
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out/lib/pkgconfig
+            mkdir -p $out/include/butter
+
+            cp ${artifact} $out/lib
+            sed -e "s|^prefix=.*|prefix=$out|" \
+              -e "s|^libdir=.*|libdir=$out/lib|" \
+              lib/${profile}/pkgconfig/${artifactStem}.pc \
+              > $out/lib/pkgconfig/${pc}
+
+            cp -r include/butter/* $out/include/butter
+
+            runHook postInstall
+          '';
+        };
     in {
-      packages.butter-wayland-release = pkgs.stdenv.mkDerivation {
-        pname = "butter-wayland";
-        version = pversion;
-
-        src = ./.;
-
-        nativeBuildInputs = [
-          conjure.packages.${system}.default
-          pkgs.gcc
-          pkgs.vulkan-loader
-          pkgs.vulkan-headers
-          pkgs.wayland
-
-          htils.packages.${system}.htils-threadsafe
-        ];
-
-        buildPhase = ''
-          runHook preBuild
-
-          conjure as wayland-release build
-
-          runHook postBuild
-        '';
-
-        installPhase = ''
-          runHook preInstall
-
-          mkdir -p $out/lib/pkgconfig
-          mkdir -p $out/include/butter
-
-          mkdir -p $out/lib/pkgconfig
-          sed -e "s|^prefix=.*|prefix=$out|" -e "s|^libdir=.*|libdir=$out/lib|" lib/wayland-release/pkgconfig/butter-wayland.pc > $out/lib/pkgconfig/butter-wayland.pc
-
-          cp lib/wayland-release/libbutter-wayland.so $out/lib
-          cp -r include/butter/* $out/include/butter
-
-          runHook postInstall
-        '';
-      };
-
-      packages.butter-wayland-release-static = pkgs.stdenv.mkDerivation {
-        pname = "butter-wayland-static";
-        version = pversion;
-
-        src = ./.;
-
-        nativeBuildInputs = [
-          conjure.packages.${system}.default
-          pkgs.gcc
-          pkgs.vulkan-headers
-          pkgs.wayland
-
-          htils.packages.${system}.htils-threadsafe
-        ];
-
-        buildPhase = ''
-          runHook preBuild
-
-          conjure as wayland-release-static build
-
-          runHook postBuild
-        '';
-
-        installPhase = ''
-          runHook preInstall
-
-          mkdir -p $out/lib/pkgconfig
-          mkdir -p $out/include/butter
-          sed -e "s|^prefix=.*|prefix=$out|" -e "s|^libdir=.*|libdir=$out/lib|" lib/wayland-release-static/pkgconfig/butter-wayland-static.pc > $out/lib/pkgconfig/butter-wayland-static.pc
-
-          cp lib/wayland-release-static/libbutter-wayland.a $out/lib
-          cp -r include/butter/* $out/include/butter
-
-          runHook postInstall
-        '';
-      };
-
-      packages.butter-wayland-debug = pkgs.stdenv.mkDerivation {
-        pname = "butter-wayland-debug";
-        version = pversion;
-
-        src = ./.;
-
-        nativeBuildInputs = [
-          conjure.packages.${system}.default
-          pkgs.gcc
-          pkgs.vulkan-headers
-          pkgs.wayland
-
-          htils.packages.${system}.htils-debug-threadsafe
-        ];
-
-        buildPhase = ''
-          runHook preBuild
-
-          conjure as wayland-debug build
-
-          runHook postBuild
-        '';
-
-        installPhase = ''
-          runHook preInstall
-
-          mkdir -p $out/lib/pkgconfig
-          mkdir -p $out/include/butter
-
-          sed -e "s|^prefix=.*|prefix=$out|" -e "s|^libdir=.*|libdir=$out/lib|" lib/wayland-debug/pkgconfig/butter-wayland-debug.pc > $out/lib/pkgconfig/butter-wayland-debug.pc
-          cp lib/wayland-debug/libbutter-wayland-debug.a $out/lib
-          cp -r include/butter/* $out/include/butter
-
-          runHook postInstall
-        '';
-      };
-
-      packages.butter-x11-release = pkgs.stdenv.mkDerivation {
-        pname = "butter-x11";
-        version = pversion;
-
-        src = ./.;
-
-        nativeBuildInputs = [
-          conjure.packages.${system}.default
-          pkgs.gcc
-          pkgs.vulkan-loader
-          pkgs.vulkan-headers
-          pkgs.libxcb
-
-          htils.packages.${system}.htils-threadsafe
-        ];
-
-        buildPhase = ''
-          runHook preBuild
-
-          conjure as x11-release build
-
-          runHook postBuild
-        '';
-
-        installPhase = ''
-          runHook preInstall
-
-          mkdir -p $out/lib/pkgconfig
-          mkdir -p $out/include/butter
-
-          sed -e "s|^prefix=.*|prefix=$out|" -e "s|^libdir=.*|libdir=$out/lib|" lib/x11-release/pkgconfig/butter-x11.pc > $out/lib/pkgconfig/butter-x11.pc
-          cp lib/x11-release/libbutter-x11.so $out/lib
-          cp -r include/butter/* $out/include/butter
-
-          runHook postInstall
-        '';
-      };
-
-      packages.butter-x11-release-static = pkgs.stdenv.mkDerivation {
-        pname = "butter-x11-static";
-        version = pversion;
-
-        src = ./.;
-
-        nativeBuildInputs = [
-          conjure.packages.${system}.default
-          pkgs.gcc
-          pkgs.vulkan-headers
-          pkgs.libxcb
-
-          htils.packages.${system}.htils-threadsafe
-        ];
-
-        buildPhase = ''
-          runHook preBuild
-
-          conjure as x11-release-static build
-
-          runHook postBuild
-        '';
-
-        installPhase = ''
-          runHook preInstall
-
-          mkdir -p $out/lib/pkgconfig
-          mkdir -p $out/include/butter
-
-          sed -e "s|^prefix=.*|prefix=$out|" -e "s|^libdir=.*|libdir=$out/lib|" lib/x11-release-static/pkgconfig/butter-x11-static.pc > $out/lib/pkgconfig/butter-x11-static.pc
-          cp lib/x11-release-static/libbutter-x11.a $out/lib
-          cp -r include/butter/* $out/include/butter
-
-          runHook postInstall
-        '';
-      };
-
-      packages.butter-x11-debug = pkgs.stdenv.mkDerivation {
-        pname = "butter-x11-debug";
-        version = pversion;
-
-        src = ./.;
-
-        nativeBuildInputs = [
-          conjure.packages.${system}.default
-          pkgs.gcc
-          pkgs.vulkan-headers
-          pkgs.libxcb
-
-          htils.packages.${system}.htils-debug-threadsafe
-        ];
-
-        buildPhase = ''
-          runHook preBuild
-
-          conjure as x11-debug build
-
-          runHook postBuild
-        '';
-
-        installPhase = ''
-          runHook preInstall
-
-          mkdir -p $out/lib/pkgconfig
-          mkdir -p $out/include/butter
-
-          sed -e "s|^prefix=.*|prefix=$out|" -e "s|^libdir=.*|libdir=$out/lib|" lib/x11-debug/pkgconfig/butter-x11-debug.pc > $out/lib/pkgconfig/butter-x11-debug.pc
-          cp lib/x11-debug/libbutter-x11-debug.a $out/lib
-          cp -r include/butter/* $out/include/butter
-
-          runHook postInstall
-        '';
+      packages = {
+        butter-wayland-release = mkButter {
+          name = "butter-wayland";
+          profile = "wayland-release";
+          artifact = "lib/wayland-release/libbutter-wayland.so";
+          pc = "butter-wayland.pc";
+          platform = "wayland";
+          loader = true;
+        };
+
+        butter-wayland-release-static = mkButter {
+          name = "butter-wayland-static";
+          profile = "wayland-release-static";
+          artifact = "lib/wayland-release-static/libbutter-wayland.a";
+          pc = "butter-wayland-static.pc";
+          platform = "wayland";
+        };
+
+        butter-wayland-debug = mkButter {
+          name = "butter-wayland-debug";
+          profile = "wayland-debug";
+          artifact = "lib/wayland-debug/libbutter-wayland-debug.a";
+          pc = "butter-wayland-debug.pc";
+          platform = "wayland";
+          debug = true;
+        };
+
+        butter-x11-release = mkButter {
+          name = "butter-x11";
+          profile = "x11-release";
+          artifact = "lib/x11-release/libbutter-x11.so";
+          pc = "butter-x11.pc";
+          platform = "x11";
+          loader = true;
+        };
+
+        butter-x11-release-static = mkButter {
+          name = "butter-x11-static";
+          profile = "x11-release-static";
+          artifact = "lib/x11-release-static/libbutter-x11.a";
+          pc = "butter-x11-static.pc";
+          platform = "x11";
+        };
+
+        butter-x11-debug = mkButter {
+          name = "butter-x11-debug";
+          profile = "x11-debug";
+          artifact = "lib/x11-debug/libbutter-x11-debug.a";
+          pc = "butter-x11-debug.pc";
+          platform = "x11";
+          debug = true;
+        };
       };
 
       devShells.default = pkgs.mkShell {
